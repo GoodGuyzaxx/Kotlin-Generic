1ba0b43c471e362532a1003fded45a8c4522f304
GoodGuyzaxx <drawww111@gmail.com>
2023-01-26 13:59:53 +0900

    update: adding new files

--- src/main/kotlin/app/Covariant.kt
+++ src/main/kotlin/app/Covariant.kt
@@ -1,5 +1,11 @@
 package app
+class Covariant<out T>(val data: T){
+    fun out(): T{
+        return data
+    }
+}
 
 fun main() {
-
+    val data: Covariant<String> = Covariant("zax")
+    println(data.out())
 }
